1ed0e6afec3e40a87e76b58c4b6e4a86286374a3
Demetrio Quiñones <demetriorqe@gmail.com>
2021-09-22 15:54:45 -0400

    Creacion de una clase base e instalacion de fody para no tener que especificar OnPropertyChanged

---
+++ MultApp/MultApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MultApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MultApp.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public IAlertService AlertService { get; }
+        public INavigationService NavigationService { get; }
+
+        protected BaseViewModel(IAlertService alertService, INavigationService navigationService)
+        {
+            AlertService = alertService;
+            NavigationService = navigationService;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
