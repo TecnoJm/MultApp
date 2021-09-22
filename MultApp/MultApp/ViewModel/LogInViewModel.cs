f7c3e7dcb8d034f648f5af080b73f41d810a33a3
Demetrio Quiñones <demetriorqe@gmail.com>
2021-09-22 15:57:53 -0400

    LogInViewModel renombrado e adaptado para implementar la clase base

---
+++ MultApp/MultApp/ViewModel/LogInViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using MultApp.View;
+using MultApp.Services;
+
+namespace MultApp.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Action DisplayInvalidLoginPrompt;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICommand SubmitCommand { protected set; get; }
+        public LoginViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        public async void OnSubmit()
+        {
+            //Codigo de pruebas de ingreso
+            if (Email == "Usuario" && Password == "123")
+            {
+                var mainScreen = new MainScreen();
+                NavigationPage.SetHasNavigationBar(mainScreen, false);
+                await NavigationService.NavigationAsync(mainScreen);
+            }
+            else
+            {
+                await AlertService.AlertAsync("Error", "Usuario o Contraseña Invalidos");
+            }
+        }
+    }
+}
