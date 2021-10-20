f532f40cf5711fef156944288799f67f8b3b7930
Demetrio Quiñones <demetriorqe@gmail.com>
2021-10-19 21:36:54 -0400

    Agregada la navegacion entre el menu principal y las distintas pantallas de la aplicacion

--- MultApp/MultApp/ViewModel/MainScreenViewModel.cs
+++ MultApp/MultApp/ViewModel/MainScreenViewModel.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using MultApp.Services;
+using MultApp.View;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MultApp.ViewModel
 {
-    class MainScreenViewModel
+    public class MainScreenViewModel : BaseViewModel
     {
+        public ICommand VerEstadoCommand { get; }
+        public ICommand EscribirMultaCommand { get; }
+        public ICommand ListaMultaCommand { get; }
+        public MainScreenViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
+        {
+            VerEstadoCommand = new Command(OnVerEstado);
+            EscribirMultaCommand = new Command(OnEscribirMulta);
+            ListaMultaCommand = new Command(OnListaMulta);
+        }
+        private async void OnVerEstado()
+        {
+            await NavigationService.NavigationAsync(new VerEstadoMultaScreen(), false);
+        }
+        private async void OnEscribirMulta()
+        {
+            await NavigationService.NavigationAsync(new EscribirMultaScreen(), false);
+
+        }
+        private async void OnListaMulta()
+        {
+            await NavigationService.NavigationAsync(new ListaMultaScreen(), false);
+
+        }
 
     }
 }
