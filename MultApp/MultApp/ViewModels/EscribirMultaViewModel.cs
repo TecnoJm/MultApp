152d2bb27ebd9a79dad8ba03195ae339f0a20e84
TecnoJm <jmichael1802@hotmail.com>
2021-10-23 14:25:43 -0400

    Registro de Infraccion realizado para demostracion.

--- MultApp/MultApp/ViewModels/EscribirMultaViewModel.cs
+++ MultApp/MultApp/ViewModels/EscribirMultaViewModel.cs
@@ -11,8 +11,12 @@ namespace MultApp.ViewModels
     public class EscribirMultaViewModel : BaseViewModel
     {
         public ObservableCollection<string> LeyInfringida { get; set; }
+        public int InfraccionSeleccionada { get; set; }
+
         public ICommand RegistrarInfraccionCommand { get; }
         public ICommand VolverCommand { get; }
+        public string UserLicencia { get; set; }
+        public int NumeroLey { get; set; }
 
         public EscribirMultaViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
         {
@@ -29,42 +33,33 @@ namespace MultApp.ViewModels
         private async void OnRegistrarInfraccion()
         {
             try
-            {
-                /*
-                string serverdbname = "MultAppDB";
-                string servername = "192.168.0.14";
-                string serveruser = "Test";
-                string serverpasword = "123456";
+            {        
+                 string serverdbname = "MultAppDB";
+                 string servername = "192.168.0.14";
+                 string serveruser = "Test";
+                 string serverpasword = "123456";
 
-                string sqlconnectionstring = $"Data Source = {servername}; Initial Catalog = {serverdbname}; User Id={serveruser}; Password={serverpasword};";
-                SqlConnection sqlconnection = new SqlConnection(sqlconnectionstring);
+                 string sqlconnectionstring = $"Data Source = {servername}; Initial Catalog = {serverdbname}; User Id={serveruser}; Password={serverpasword};";
+                 SqlConnection sqlconnection = new SqlConnection(sqlconnectionstring);
 
-                //Abrir y cerrar la base de datos, siempre realizable mientas usemos la base de datos
-                sqlconnection.Open();
-                sqlconnection.Close();
+                 //Abrir y cerrar la base de datos, siempre realizable mientas usemos la base de datos
+                 sqlconnection.Open();
+                 sqlconnection.Close();
 
-                using (SqlConnection con = new SqlConnection(sqlconnectionstring))
-                {
-                    con.Open();
-                    //Prueba de Insert SQL Test de Registro de Infraccion
-                    using (SqlCommand sqlcmd = new SqlCommand("INSERT INTO values ()", con))
-                    {
-                        SqlDataReader datareader = sqlcmd.ExecuteReader();
+                 using (SqlConnection con = new SqlConnection(sqlconnectionstring))
+                  {
+                      con.Open();
+                      //Prueba de Insert SQL Test de Registro de Infraccion
+                      using (SqlCommand sqlcmd = new SqlCommand("SET IDENTITY_INSERT dbo.Penalty ON; Insert Into dbo.Penalty (Id, Seq, Code, PersonId, PenaltyTypeId, Description, AddressLine1, ProvinceId, PenaltyDate , Paid, CreatedDate) values((Select MAX(id) + 1 as num from dbo.Penalty), 7, 12, (Select Id from dbo.Person where DocumentNumber = @NumeroDocumento), @LeyInfringida, 'Se ha saltado un semaforo', 'Campos', 1, '2021-10-20 17:18:00.680', 1, '2021-10-20 17:18:00.680') ", con))
+                      {
+                          sqlcmd.Parameters.Add(new SqlParameter("NumeroDocumento", UserLicencia));
+                          sqlcmd.Parameters.Add(new SqlParameter("LeyInfringida", InfraccionSeleccionada));
+                          sqlcmd.ExecuteNonQuery();
+                      }
+                      con.Close();
+                  }
 
-                        if (datareader.Read())
-                        {
-                            await AlertService.AlertAsync("!", "Infraccion Colocada");
-                        }
-                        else
-                        {
-                            await AlertService.AlertAsync("Error", "Usuario o Contraseña incorrecta");
-                        }
-                    }
-                    con.Close();
-                }
-                */
-                await AlertService.AlertAsync("Error", "Infraccion Colocada");
-
+                await AlertService.AlertAsync("!", "Infraccion Colocada");
             }
             catch (Exception ex)
             {
--- MultApp/MultApp/ViewModels/VerEstadoMultaViewModel.cs
+++ MultApp/MultApp/ViewModels/VerEstadoMultaViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -9,10 +10,44 @@ namespace MultApp.ViewModels
 {
     public class VerEstadoMultaViewModel : BaseViewModel
     {
+        public string UserLicencia { get; set; }
+        public string UserNombres { get; set; }
+        public string UserApellidos { get; set; }
+        public string UserNacimiento { get; set; }
+
+        public class MyTableList
+        {
+            public string DocumentoLicencia { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public string Nacimiento { get; set; }
+        }
+
         public ICommand VolverCommand { get; }
         public VerEstadoMultaViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
         {
             VolverCommand = new Command(OnVolver);
+            //
+            string serverdbname = "MultAppDB";
+            string servername = "192.168.0.14";
+            string serveruser = "Test";
+            string serverpasword = "123456";
+
+            string sqlconnectionstring = $"Data Source = {servername}; Initial Catalog = {serverdbname}; User Id={serveruser}; Password={serverpasword};";
+            SqlConnection sqlconnection = new SqlConnection(sqlconnectionstring);
+
+            //Abrir y cerrar la base de datos, siempre realizable mientas usemos la base de datos
+            sqlconnection.Open();
+            
+            string queryString = "Select * from dbo.Person";
+            SqlCommand sqlcommand = new SqlCommand(queryString, sqlconnection);
+            SqlDataReader sqlreader = sqlcommand.ExecuteReader();
+            while (sqlreader.Read())
+            {
+
+            }
+
+            sqlconnection.Close();
         }
 
         private async void OnVolver()
