423a42f234b49ba4782f8961b8e25224251518e3
TecnoJm <jmichael1802@hotmail.com>
2021-10-25 18:56:03 -0400

    Buscador de persona agregada (Para demostracion).

--- MultApp/MultApp/ViewModels/VerEstadoMultaViewModel.cs
+++ MultApp/MultApp/ViewModels/VerEstadoMultaViewModel.cs
@@ -10,44 +10,51 @@ namespace MultApp.ViewModels
 {
     public class VerEstadoMultaViewModel : BaseViewModel
     {
+        public string UserBuscarLicencia { get; set; }
         public string UserLicencia { get; set; }
         public string UserNombres { get; set; }
         public string UserApellidos { get; set; }
-        public string UserNacimiento { get; set; }
-
-        public class MyTableList
-        {
-            public string DocumentoLicencia { get; set; }
-            public string Nombre { get; set; }
-            public string Apellidos { get; set; }
-            public string Nacimiento { get; set; }
-        }
 
         public ICommand VolverCommand { get; }
+        public ICommand BuscarEstado { get; }
         public VerEstadoMultaViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
         {
             VolverCommand = new Command(OnVolver);
-            //
-            string serverdbname = "MultAppDB";
-            string servername = "192.168.0.14";
-            string serveruser = "Test";
-            string serverpasword = "123456";
+            BuscarEstado = new Command(OnBuscarEstado);
+        }
 
-            string sqlconnectionstring = $"Data Source = {servername}; Initial Catalog = {serverdbname}; User Id={serveruser}; Password={serverpasword};";
-            SqlConnection sqlconnection = new SqlConnection(sqlconnectionstring);
-
-            //Abrir y cerrar la base de datos, siempre realizable mientas usemos la base de datos
-            sqlconnection.Open();
-            
-            string queryString = "Select * from dbo.Person";
-            SqlCommand sqlcommand = new SqlCommand(queryString, sqlconnection);
-            SqlDataReader sqlreader = sqlcommand.ExecuteReader();
-            while (sqlreader.Read())
+        private async void OnBuscarEstado()
+        {
+            try
             {
+                string serverdbname = "MultAppDB";
+                string servername = "192.168.0.14";
+                string serveruser = "Test";
+                string serverpasword = "123456";
 
+                string sqlconnectionstring = $"Data Source = {servername}; Initial Catalog = {serverdbname}; User Id={serveruser}; Password={serverpasword};";
+                SqlConnection sqlconnection = new SqlConnection(sqlconnectionstring);
+
+                //Consulta de Test para poder ver el estado de un Usuario
+                sqlconnection.Open();
+
+                string queryString = "Select * from dbo.Person where DocumentNumber = " + UserBuscarLicencia;
+                SqlCommand sqlcommand = new SqlCommand(queryString, sqlconnection);
+                SqlDataReader sqlreader = sqlcommand.ExecuteReader();
+
+                while (sqlreader.Read())
+                {
+                    UserNombres = sqlreader["FirstName"].ToString();
+                    UserApellidos = sqlreader["LastName"].ToString();
+                    UserLicencia = sqlreader["DocumentNumber"].ToString();
+                }
+
+                sqlconnection.Close();
             }
-
-            sqlconnection.Close();
+            catch(Exception ex)
+            {
+                await AlertService.AlertAsync("Aviso", "Información incorrecta");
+            }
         }
 
         private async void OnVolver()
