440cd27169f2b09c3e6e5b5f55ec8c79181e6df5
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-03 15:35:39 -0400

    Modelo Ley Añadido

---
+++ MultApp/MultApp/Models/Ley.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MultApp.Models
+{
+    public class Ley
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Law")]
+        public string NumeroLey { get; set; }
+
+        [JsonProperty("Description")]
+        public string Descripcion { get; set; }
+
+        [JsonProperty("Price")]
+        public int Precio { get; set; }
+    }
+}
