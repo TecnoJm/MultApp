d3c3cab07ff114cc02dddd2c6ad69f87f5f8e867
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-11 16:51:51 -0400

    Ahora es posible ver la lista de multas de un conductor

--- MultApp/MultApp/ViewModels/ListaMultaViewModel.cs
+++ MultApp/MultApp/ViewModels/ListaMultaViewModel.cs
@@ -3,19 +3,42 @@ using MultApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
+
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MultApp.ViewModels
 {
     public class ListaMultaViewModel : BaseViewModel
     {
         public ObservableCollection<Multa> Multas { get; set; }
-        public ListaMultaViewModel(IAlertService alertService, INavigationService navigationService) : base(alertService, navigationService)
+        public Persona Persona { get; }
+        public IPenaltyApiService PenaltyApiService { get; }
+        public ICommand TappedCommand { get; }
+        public ListaMultaViewModel(IAlertService alertService, INavigationService navigationService, IPenaltyApiService penaltyApiService, Persona persona) : base(alertService, navigationService)
         {
-            Multas = new ObservableCollection<Multa>()
+            PenaltyApiService = penaltyApiService;
+            Persona = persona;
+            GetMultas();
+            TappedCommand = new Command<Multa>(OnTapped);
+        }
+        private async void GetMultas()
+        {
+            await RunIsBusyTaskAsync(async () =>
             {
+                IsApiBusy = true;
+                Multas = await PenaltyApiService.GetPenailtiesByPersonIdAsync(Persona.Id);
+                IsApiBusy = false;
 
-            };
+            });
+        }
+
+        private async void OnTapped(Multa multa)
+        {
+            await RunIsBusyTaskAsync(async () =>
+            {
+                await AlertService.AlertAsync("!", multa.Description);
+            });
         }
     }
 }
--- MultApp/MultApp/Views/ListaMultaScreen.xaml.cs
+++ MultApp/MultApp/Views/ListaMultaScreen.xaml.cs
@@ -1,23 +1,25 @@
-﻿using MultApp.Services;
+﻿using MultApp.Models;
+using MultApp.Services;
 using MultApp.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit;
 
 namespace MultApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListaMultaScreen : ContentPage
     {
-        public ListaMultaScreen()
+        public ListaMultaScreen(Persona persona)
         {
             InitializeComponent();
-            BindingContext = new ListaMultaViewModel(new AlertService(), new NavigationService());
+            BindingContext = new ListaMultaViewModel(new AlertService(), new NavigationService(), new PenaltyApiService(), persona);
 
         }
 
