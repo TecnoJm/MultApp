a9abf5e5d9711af57b3fade815d223c45b94ad62
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-11 16:54:56 -0400

    Paquete BuildTools removido ahora simplemente basta con poner la ApiKey en su propiedad correspondiente con el fin de correr el programa

--- MultApp/MultApp/Config.cs
+++ MultApp/MultApp/Config.cs
@@ -1,5 +1,4 @@
-﻿using MultApp.Helpers;
-using MultApp.Models;
+﻿using MultApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,7 +7,8 @@ namespace MultApp
 {
     public static class Config
     {
-        public const string ApiKey = Secrets.ApiKey;
+        public const string ApiKey = "AIzaSyA1s3jcQ0XKC-1HazAxpvBNwXEM3jmzYBo";
+        //public const string ApiKey = "Api Key Here";
 
 
         public const string ApiUrl = "https://multapp-api.herokuapp.com/api";
