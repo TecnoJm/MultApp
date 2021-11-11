a3c5994d78b65f6c164d55041cc7d3d52f9f78d0
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-11 16:50:39 -0400

    Primera iteracion de la pantalla principal para un conductor creada

---
+++ MultApp/MultApp/ViewModels/MainScreenConductorViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MultApp.Models;
+using MultApp.Services;
+using MultApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MultApp.ViewModels
+{
+    public class MainScreenConductorViewModel : BaseViewModel
+    {
+        public IPenaltyApiService PenaltyApiService { get; }
+        public Persona Persona { get; }
+        public ICommand ListaMultaCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        public MainScreenConductorViewModel(IAlertService alertService, INavigationService navigationService, IPenaltyApiService penaltyApiService, Persona persona) : base(alertService, navigationService)
+        {
+            Persona = persona;
+            PenaltyApiService = penaltyApiService;
+            ListaMultaCommand = new Command(OnListaMulta);
+            LogoutCommand = new Command(OnLogout);
+        }
+        private async void OnListaMulta()
+        {
+            await RunIsBusyTaskAsync(async () =>
+            {
+                await NavigationService.NavigationAsync(new ListaMultaScreen(Persona), false);
+            });
+
+        }
+
+        private async void OnLogout()
+        {
+            await RunIsBusyTaskAsync(async () =>
+            {
+                await NavigationService.NavigationPopAsync();
+            });
+
+        }
+    }
+}
---
+++ MultApp/MultApp/Views/MainScreenConductor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MultApp.Models;
+using MultApp.Services;
+using MultApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainScreenConductor : ContentPage
+    {
+        public MainScreenConductor(Persona persona)
+        {
+            InitializeComponent();
+            BindingContext = new MainScreenConductorViewModel(new AlertService(), new NavigationService(), new PenaltyApiService(), persona);
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
