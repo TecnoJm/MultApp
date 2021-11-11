7fabd171c87ea57373c5d14410469d95b08ad5bd
Demetrio Quiñones <demetriorqe@gmail.com>
2021-11-11 16:47:43 -0400

    Renombrado MainScreenViewModel a MainScreenAgenteViewModel

--- MultApp/MultApp/ViewModels/MainScreenAgenteViewModel.cs
+++ MultApp/MultApp/ViewModels/MainScreenAgenteViewModel.cs
@@ -3,13 +3,12 @@ using MultApp.Services;
 using MultApp.Views;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MultApp.ViewModels
 {
-    public class MainScreenViewModel : BaseViewModel
+    public class MainScreenAgenteViewModel : BaseViewModel
     {
         public bool IsEnabled { get; set; } = false;
         public string DocumentoDeIdentidad { get; set; }
@@ -17,17 +16,17 @@ namespace MultApp.ViewModels
         public IPersonApiService PersonApiService { get; set; }
         public ICommand VerEstadoCommand { get; }
         public ICommand EscribirMultaCommand { get; }
-        public ICommand ListaMultaCommand { get; }
         public ICommand BuscarPersona { get; }
+        public ICommand LogoutCommand { get; }
 
 
 
-        public MainScreenViewModel(IAlertService alertService, INavigationService navigationService, IPersonApiService personApiService) : base(alertService, navigationService)
+        public MainScreenAgenteViewModel(IAlertService alertService, INavigationService navigationService, IPersonApiService personApiService) : base(alertService, navigationService)
         {
             VerEstadoCommand = new Command(OnVerEstado);
             EscribirMultaCommand = new Command(OnEscribirMulta);
-            ListaMultaCommand = new Command(OnListaMulta);
             BuscarPersona = new Command(OnBuscarPersona);
+            LogoutCommand = new Command(OnLogout);
             PersonApiService = personApiService;
         }
         private async void OnVerEstado()
@@ -45,21 +44,15 @@ namespace MultApp.ViewModels
             });
 
         }
-        private async void OnListaMulta()
-        {
-            await RunIsBusyTaskAsync(async () =>
-            {
-                await NavigationService.NavigationAsync(new ListaMultaScreen(), false);
-            });
 
-        }
         private async void OnBuscarPersona()
         {
-            IsApiBusy = true;
             await RunIsBusyTaskAsync(async () =>
             {
+                IsApiBusy = true;
+
                 IsEnabled = false;
-                Persona = await PersonApiService.GetPersonAsync(DocumentoDeIdentidad);
+                Persona = await PersonApiService.GetPersonByDocumentAsync(DocumentoDeIdentidad);
                 if (Persona == null)
                 {
                     await AlertService.AlertAsync("Error", "El documento ingresado no es valido");
@@ -69,8 +62,19 @@ namespace MultApp.ViewModels
                     await AlertService.AlertAsync("Success", $"Persona encontrada: {Persona.Nombre} {Persona.Apellido}");
                     IsEnabled = true;
                 }
+                IsApiBusy = false;
+
             });
-            IsApiBusy = false;
+
+        }
+
+
+        private async void OnLogout()
+        {
+            await RunIsBusyTaskAsync(async () =>
+            {
+                await NavigationService.NavigationPopAsync();
+            });
 
         }
 
