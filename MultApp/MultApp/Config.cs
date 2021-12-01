be8d5c18b9993e79a1b02ef1d6c4849232166a34
Demetrio Quiñones <demetriorqe@gmail.com>
2021-12-01 18:30:55 -0400

    Agregada configuracion para la navegacion y parametros con prism

--- MultApp/MultApp/Config.cs
+++ MultApp/MultApp/Config.cs
@@ -7,13 +7,25 @@ namespace MultApp
 {
     public static class Config
     {
-        //public const string ApiKey = "Api Key Here";
-
+        public const string ApiKey = "AIzaSyA1s3jcQ0XKC-1HazAxpvBNwXEM3jmzYBo";
 
         public const string ApiUrl = "https://multapp-api.herokuapp.com/api";
 
         public static ObservableCollection<Ley> Leyes { get; set; }
         public static ObservableCollection<Provincia> Provincias { get; set; }
 
+        //Navigation Pages name
+        public const string MainScreenAgente = "MainAgente";
+        public const string MainScreenConductor = "MainCounductor";
+        public const string VerEstadoMultaScreen = "EstadoMulta";
+        public const string EscribirMultaScreen = "EscribirMuta";
+        public const string ListaMultaScreen = "ListMulta";
+        public const string LoginScreen = "Login";
+        public const string RegisterScreen = "Register";
+        public const string NavigationPage = "Navigation";
+
+        //Navigation Parameters name
+        public const string PersonaParam = "Persona";
+        public const string UsuarioParam = "Usuario";
     }
 }
